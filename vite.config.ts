e575102bb0007fdda323f49f8d634904b2311d9a
Fra46 <andresfzapatamar@gmail.com>
2026-07-18 01:13:38 -0500

    feat: agregar soporte para HTTPS y actualizar el manifiesto de la aplicación

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    https: true
   },
   resolve: {
     alias: {
